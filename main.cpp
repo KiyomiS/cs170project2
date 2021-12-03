eb32ddbb613372dc85310dd55f7d1e2ac0d88215
KiyomiS <62919233+KiyomiS@users.noreply.github.com>
2021-12-03 02:37:11 -0800

    Starting up project

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+    cout << "Welcome to my project 2." << endl;
     
+    cout << "Which data set would you like to test?"\n << "1. Small\n" << "2. Large.\n << endl;
+    cin >> input;
+    
+    if (choice == 1) {
+    }
+    else if (choice == 2) {
+    } else {
+        cout << "Invalid choice. Will use default choice (small)" << endl;
+    }
 }
