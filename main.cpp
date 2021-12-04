44b676eb6d4d7bbee0602e38eaa5836459619feb
KiyomiS <kiyomis@ymail.com>
2021-12-03 22:29:06 -0800

    still starting up

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main() {
+    int input;
+
     cout << "Welcome to my project 2." << endl;
     
-    cout << "Which data set would you like to test?"\n << "1. Small\n" << "2. Large.\n << endl;
+    cout << "Which data set would you like to test?\n" << "1. Small\n" << "2. Large.\n" << endl;
     cin >> input;
     
-    if (choice == 1) {
+    if (input == 1) {
     }
-    else if (choice == 2) {
+    else if (input == 2) {
     } else {
         cout << "Invalid choice. Will use default choice (small)" << endl;
     }
